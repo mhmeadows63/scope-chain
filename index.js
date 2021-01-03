ee051ea3d29c9d8fe6b6ed636165ab17839b7015
Martin Meadows <martin@netconfigs.com>
2021-01-03 17:29:12 +0000

    enhancement for string-final and context-attributes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-module.exports = function chain(next/*, task, task, ...*/) {
-    var next = next || function (err) { err && console.error(err.stack || err) };
-    var tasks = Array.prototype.slice.call(arguments, 1);
+module.exports = function chain(/*?ctx, next, task, task, ...*/) {
+    var tasks = Array.prototype.slice.call(arguments),
+        ctx = tasks[0] && typeof tasks[0] === 'object' && tasks.shift(), // typeof null is 'object'
+        next = tasks.shift();
+    if (!next)
+        next = function next(err) { err && console.error(err.stack || err) };
+    else if (typeof next === 'string')
+        next = function next(err) { err && console.error(this + (err.stack || err)) }.bind(next + ' ');
+    for (var key in ctx)
+        next[key] = ctx[key];
+
     var funcs = [
         // function that considers the 1st argument as a potential error to forward
         // on-error - abort the chain and return the error as an argument to next()
@@ -38,14 +46,14 @@ module.exports = function chain(next/*, task, task, ...*/) {
             catch (ex) { next(ex) } // return any thrown exception as the argument to next()
         },
     ];
-    
-    // copy any properties on the 'next' function to the all functions
+
+    // copy any enumerable properties from the 'next' function to all functions
     funcs.forEach(function (func, i, a) {
         for (var key in next) func[key] = next[key]; // copy next.* to each func
         if (i) this[func.name] = func; // copy silent, ignore, noerror to done
         else this.this = next; // copy next to done.this
     }, funcs[0]);
-    
+
     try { tasks.length ? tasks.shift().apply(funcs[0]) : next() } // invoke the 1st task with zero-arguments
     catch (ex) { next(ex) } // return any thrown exception as the argument to next()
 }
--- test.js
+++ test.js
@@ -1,37 +1,97 @@
 var chain = require('.');
+var trace = console.log || function() {};
 
-var error;
+chain(function conclusion(err) {
+    err ? console.log(err) : trace('SUCCESS')
+    process.exit(!!err);
 
-function conclusion(err) {
-    if (err) code = console.error(error = err);
-    process.exit(!!error);
-}
+}, function () {
+    trace('this()');
+    process.nextTick(this, null, 'a', 'b', 'c');
 
-chain(conclusion, function step0() {
-    if (conclusion !== this.this) console.log(error = 'wrong this.this');
+}, function test(a, b, c) { // test regular arg delivery to following step
+    if (a != 'a' || b != 'b' || c != 'c')
+        console.log('this() - failed');
+    this();
+
+}, function test() { // test parsing of string as conclusion
+    trace('string');
+    var next = this;
+    var error = console.error;
+    console.error = function (err) {
+        console.error = error;
+        if (err.startsWith('test: ') === false)
+            console.log('final-string - failed');
+        next();
+    };
+    chain('test:', function () { JSON.parse(';') });
+
+}, function test() { // test extra context
+    trace('context');
+    chain({ attr: 42 }, this, function () {
+        if (this.attr !== 42)
+            console.log('extra context - failed');
+        this();
+
+    }, function () {
+        if (this.attr !== 42)
+            console.log('extra context - failed');
+        this();
+
+    });
+    
+}, function test() { // test null _final_
+    trace('null-final');
+    chain(null, function () {
+        this();
+
+    });
+    this();
+
+} , function test() { // test delivery of next to recursing chains
+    trace('recursion');
+    var conclusion = this.this;
     chain(this, function () {
-        if (conclusion !== this.this.this) console.log(error = 'wrong this.this.this');
+        if (conclusion !== this.this.this)
+            console.log('wrong this.this.this');
         chain(this, function () {
-            if (conclusion !== this.this.this.this) console.log(error = 'wrong this.this.thisi.this');
+            if (conclusion !== this.this.this.this)
+                console.log('wrong this.this.thisi.this');
+            this();
         });
     });
 
-}, function step1() {
-    process.nextTick(this, null, 'a', 'b', 'c');
+}, function test() { // test this.noerror() delivery of all args to next step
+    trace('this.noerror()');
+    chain(this, function () {
+        process.nextTick(this.noerror, new Error('this error should have been delivered to the next task'), 'a', 'b', 'c');
 
-}, function step2(a, b, c) {
-    if (a != 'a' || b != 'b' || c != 'c') console.log(error = 'this - failed');
-    process.nextTick(this.noerror, new Error('this error should have been delivered to the next task'), 'a', 'b', 'c');
+    }, function (err, a, b, c) {
+        if (!err || a != 'a' || b != 'b' || c != 'c')
+            console.log('this.noerror - failed');
+        this();
 
-}, function step3(err, a, b, c) {
-    if (!err || a != 'a' || b != 'b' || c != 'c') console.log(error = 'this.noerror - failed');
-    process.nextTick(this.ignore, new Error('this exception should have been ignored'), 'a', 'b', 'c');
+    });
 
-}, function step4(a, b, c) {
-    if (a != 'a' || b != 'b' || c != 'c') console.log(error = 'this.ignore - failed');
-    process.nextTick(this.silent, new Error('this exception should not have been visible'), 'a', 'b', 'c');
+}, function test() { // test this.ignore() delivery of just non-err args
+    trace('this.ignore()');
+    chain(this, function () {
+        process.nextTick(this.ignore, new Error('this exception should have been ignored'), 'a', 'b', 'c');
 
-}, function step5() {
-    console.log(error = 'this.silent - failed');
+    }, function (a, b, c) {
+        if (a != 'a' || b != 'b' || c != 'c')
+            console.log('this.ignore - failed');
+        this();
+    });
 
+}, function test(a, b, c) { // test this.silent() abort via conclusion()
+    trace('this.silent()');
+    chain(this, function () {
+        process.nextTick(this.silent, new Error('this exception should not have been visible'), 'a', 'b', 'c');
+
+    }, function () {
+        console.log('this.silent - failed');
+        this();
+
+    });
 });
